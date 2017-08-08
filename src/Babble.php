aa6f8ef9f2db39bf01f13a8c2a25d53cd7b8208c
Olav Lindekleiv <olav@lindekleiv.com>
2017-08-08 20:35:49 +0200

    Change namespace.

--- src/Babble.php
+++ src/Babble.php
@@ -7,7 +7,6 @@ use Symfony\Component\Debug\Debug;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Yaml\Yaml;
 
 class Babble
 {
